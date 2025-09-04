e79f3df4e48106ab6d1a3debe6f99fba6c759cd5
Quoc Hien <hiennqce172010@fpt.edu.vn>
2025-09-04 11:20:45 +0700

    api gateway

--- SEP490_Ezstay/AmenityAPI/DTO/Request/AmenityDto.cs
+++ SEP490_Ezstay/AmenityAPI/DTO/Request/AmenityDto.cs
@@ -7,6 +7,7 @@ public class AmenityDto
     public Guid Id { get; set; }
     public string AmenityName { get; set; } 
     public Guid StaffId { get; set; }
+    public string ImageUrl { get; set; }
     public DateTime CreatedAt { get; set; }
     public DateTime UpdatedAt { get; set; }
 }
