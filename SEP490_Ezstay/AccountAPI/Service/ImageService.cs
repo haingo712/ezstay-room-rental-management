5bbc4988608bcc7074040719281465a776928654
nqui61042-wq <nqui61042@gmail.com>
2025-09-10 13:38:57 +0700

    push AccountApi New

---
+++ SEP490_Ezstay/AccountAPI/Service/ImageService.cs
@@ -0,0 +1,14 @@
+﻿using AccountAPI.Service.Interfaces;
+
+namespace AccountAPI.Service
+{
+    public class ImageService : IImageService
+    {
+        public async Task<string> UploadImageAsync(IFormFile file)
+        {
+            // TODO: call ImageAPI, nhận lại URL
+            await Task.Delay(50);
+            return "https://cdn.myapp.com/images/" + Guid.NewGuid() + Path.GetExtension(file.FileName);
+        }
+    }
+}
