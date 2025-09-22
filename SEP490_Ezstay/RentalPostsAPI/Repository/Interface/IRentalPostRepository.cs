89067e80e52c34b69497c34f76dc7ad5d0ea17dd
Ngo Hong Hai <124362081+haingo712@users.noreply.github.com>
2025-09-22 18:25:10 +0700

    Update IRentalPostRepository.cs

--- SEP490_Ezstay/RentalPostsAPI/Repository/Interface/IRentalPostRepository.cs
+++ SEP490_Ezstay/RentalPostsAPI/Repository/Interface/IRentalPostRepository.cs
@@ -5,10 +5,13 @@ namespace RentalPostsAPI.Repository.Interface
     public interface IRentalPostRepository
     {
         Task<IEnumerable<RentalPosts>> GetAllByOwnerIdAsync(Guid ownerId);
+        IQueryable<RentalPosts> GetAllAsQueryable();
         Task<RentalPosts> CreateAsync(RentalPosts post);
         Task<RentalPosts?> GetByIdAsync(Guid id);
         Task<IEnumerable<RentalPosts>> GetAllAsync();
         Task<RentalPosts?> UpdateAsync(RentalPosts post);
         Task<bool> DeleteAsync(Guid id, Guid deletedBy);
+        
+        Task<IEnumerable<RentalPosts>> GetByRoomIdAsync(Guid roomId);
     }
 }
