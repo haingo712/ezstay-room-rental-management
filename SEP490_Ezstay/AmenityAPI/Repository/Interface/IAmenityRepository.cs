9234403bc92d8fca8663427856746d1e9bd3db38
Quoc Hien <hiennqce172010@fpt.edu.vn>
2025-10-06 21:26:28 +0700

    ok

--- SEP490_Ezstay/AmenityAPI/Repository/Interface/IAmenityRepository.cs
+++ SEP490_Ezstay/AmenityAPI/Repository/Interface/IAmenityRepository.cs
@@ -8,10 +8,13 @@ public interface IAmenityRepository
     Task<IEnumerable<Amenity>> GetAll();
   //  Task<IEnumerable<Amenity>> GetAllByStaffId(Guid staffId);
     Task<Amenity?> GetByIdAsync(Guid id);
+    
     Task AddAsync(Amenity amenity);
     Task UpdateAsync(Amenity amenity);
     Task DeleteAsync(Amenity amenity);
-    Task<bool> AmenityNameExistsAsync(string roomName);
+    Task<bool> AmenityNameExistsAsync(string amenityName);
+    Task<bool> AmenityNameExistsAsync(string amenityName, Guid id);
+
 
 
 }
