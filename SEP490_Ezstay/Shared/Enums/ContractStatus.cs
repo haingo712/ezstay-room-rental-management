a231fb7827296620a14d40ef023ad66cdf19e862
Quoc Hien <hiennqce172010@fpt.edu.vn>
2025-10-16 20:00:10 +0700

    ok

--- SEP490_Ezstay/Shared/Enums/ContractStatus.cs
+++ SEP490_Ezstay/Shared/Enums/ContractStatus.cs
@@ -2,8 +2,9 @@ namespace Shared.Enums;
 
 public enum ContractStatus
 {
-    Active = 0,
-    Evicted = 1, // Bị đuổi, vi phạm hợp đồng
-    Cancelled = 2  ,
-    Expired =3 //hợp đồng đã hết hạn
+    Active = 1,
+    Evicted = 4, // Bị đuổi, vi phạm hợp đồng
+    Cancelled = 2,
+    Expired = 3, //hợp đồng đã hết hạn
+    Pending = 0 //hợp đồng đang chờ duyệt
 }
