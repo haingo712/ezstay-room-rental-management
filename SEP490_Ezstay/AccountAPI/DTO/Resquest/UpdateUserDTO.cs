7b70ad2edf46f98cfe3d6155cc537278494aea23
Ngo Hong Hai <124362081+haingo712@users.noreply.github.com>
2025-12-07 14:13:20 +0700

    Refactor UpdateUserDTO with required fields

--- SEP490_Ezstay/AccountAPI/DTO/Resquest/UpdateUserDTO.cs
+++ SEP490_Ezstay/AccountAPI/DTO/Resquest/UpdateUserDTO.cs
@@ -1,7 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using AccountAPI.Enums;
 using Microsoft.AspNetCore.Http;
-using System.ComponentModel.DataAnnotations;
 
 namespace AccountAPI.DTO.Request
 {
@@ -9,12 +8,10 @@ namespace AccountAPI.DTO.Request
     {
         [Required(ErrorMessage = "Gender is required")]
         public GenderEnum Gender { get; set; }
-        
         public string? Avatar { get; set; }
         
-        [Required(ErrorMessage = "Bio is required")]
         [StringLength(500, ErrorMessage = "Bio cannot exceed 500 characters")]
-        public string Bio { get; set; }
+        public string? Bio { get; set; }
         
         [Required(ErrorMessage = "Date of birth is required")]
         public DateTime DateOfBirth { get; set; }
@@ -25,16 +22,30 @@ namespace AccountAPI.DTO.Request
         [Required(ErrorMessage = "Province is required")]
         public string ProvinceId { get; set; }     // Mã tỉnh
 
-        public string? WardId { get; set; }         // Mã xã/phường
-                                                    //public string WardName { get; set; }       // Tên xã/phường
-        public string? FullName { get; set; }
-        public string? FrontImageUrl { get; set; }
-        public string? BackImageUrl { get; set; }
-        public string? TemporaryResidence { get; set; }
-        [Required(ErrorMessage = "CitizenIdNumber is required")]
-        [RegularExpression(@"^\d{12}$", ErrorMessage = "CitizenIdNumber must be exactly 12 digits and numeric only")]
-        public string? CitizenIdNumber { get; set; }
-        public DateTime? CitizenIdIssuedDate { get; set; }
-        public string? CitizenIdIssuedPlace { get; set; }
+        [Required(ErrorMessage = "Ward is required")]
+        public string WardId { get; set; }         // Mã xã/phường
+                                                    
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }
+        
+        [Required(ErrorMessage = "Front image of citizen ID is required")]
+        public string FrontImageUrl { get; set; }
+        
+        [Required(ErrorMessage = "Back image of citizen ID is required")]
+        public string BackImageUrl { get; set; }
+        
+        [Required(ErrorMessage = "Temporary residence is required")]
+        public string TemporaryResidence { get; set; }
+        
+        [Required(ErrorMessage = "Citizen ID number is required")]
+        [StringLength(12, MinimumLength = 12, ErrorMessage = "Citizen ID must be exactly 12 digits")]
+        [RegularExpression(@"^\d{12}$", ErrorMessage = "Citizen ID must contain only digits")]
+        public string CitizenIdNumber { get; set; }
+        
+        [Required(ErrorMessage = "Citizen ID issued date is required")]
+        public DateTime CitizenIdIssuedDate { get; set; }
+        
+        [Required(ErrorMessage = "Citizen ID issued place is required")]
+        public string CitizenIdIssuedPlace { get; set; }
     }
 }
